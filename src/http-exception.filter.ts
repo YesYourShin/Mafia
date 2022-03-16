409dca15724a7df3cecbd4b2cb1c90a6bbafd963
MJoon-Jung <gjgjajaj31@gmail.com>
2022-03-16 15:41:47 +0900

    [FIX] 이미 로그인 한 유저 접근 시 Redirect

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -3,18 +3,30 @@ import {
   Catch,
   ArgumentsHost,
   HttpException,
+  HttpStatus,
 } from '@nestjs/common';
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
+
+    if (request.path.match(/\/api\/auth\/\w+\/login/)) {
+      return response.redirect(process.env.FRONT_URL, 307);
+    }
+
     const err = exception.getResponse() as
       | string
-      | { error: string; statusCode: 400; message: string[] };
+      | {
+          error: string;
+          statusCode: HttpStatus.BAD_REQUEST;
+          message: string[];
+        };
+
     if (typeof err !== 'string' && err.error === 'Bad Request') {
       return response.status(status).json({
         success: false,
