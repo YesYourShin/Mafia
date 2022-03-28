9def6426718959966cea86f13fa406b6308c98de
MJoon-Jung <gjgjajaj31@gmail.com>
2022-03-28 11:08:00 +0900

    [ADD] Health Check controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
@@ -10,8 +10,13 @@ import { PostModule } from './modules/post/post.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { GameRoomModule } from './modules/game-room/game-room.module';
 import { NotificationModule } from './modules/notification/notification.module';
-import { EventModule } from './modules/event/event.module';
 import * as ormconfig from '../ormconfig';
+import { RedisModules } from './modules/redis/redis.modules';
+import { GameRoomEventModule } from './modules/gateway/game-room/game-room-event.module';
+import { UserEventModule } from './modules/gateway/user/user-event.module';
+import { HealthController } from './health.controller';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -29,11 +34,15 @@ import * as ormconfig from '../ormconfig';
     UserModule,
     PostModule,
     CommentModule,
+    GameRoomEventModule,
     GameRoomModule,
     NotificationModule,
-    EventModule,
+    RedisModules,
+    UserEventModule,
+    TerminusModule,
+    HttpModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, HealthController],
   providers: [AppService],
 })
 export class AppModule implements NestModule {
