9f43c524a3045c074b83f28967f78ee9d2d906f6
seon0522 <iabc0005@daum.net>
2022-03-28 13:07:03 +0900

    grant job event test

--- src/modules/event/game/game.gateway.ts
+++ src/modules/event/game/game.gateway.ts
@@ -11,8 +11,6 @@ import {
 } from '@nestjs/websockets';
 import { InjectRedis, Redis } from '@svtslv/nestjs-ioredis';
 import { Server, Socket } from 'socket.io';
-import { GamePrefix } from 'src/modules/game-room/constants';
-import { UserProfileInGame } from 'src/modules/game-room/dto';
 import { REDIS_GAME } from 'src/modules/redis';
 import { UserProfile } from 'src/modules/user/dto';
 import { Event } from './constants';
@@ -138,117 +136,6 @@ export class GameGateway
     }
   }
 
-  // -----------------------------------------------------------------------------
-  // // 방 입장
-  // @SubscribeMessage(Event.JOIN)
-  // async handleJoin(
-  //   @MessageBody() data: { user: UserProfile; gameNumber: number },
-  //   @ConnectedSocket() socket: Socket,
-  // ) {
-  //   const newNamespace = socket.nsp;
-  //   socket.join(`${socket.nsp.name}-${data.gameNumber}`);
-  //   // const members = await this.gameService.findMembers(data.gameNumber);
-  //   // newNamespace.emit(Event.ONLINELIST, members);
-  // }
-
-  // 방 준비
-  @SubscribeMessage(Event.READY)
-  async handleReady(
-    @MessageBody() data: { user: UserProfile; gameNumber: number },
-    @ConnectedSocket() socket: Socket,
-  ) {
-    const user = JSON.stringify(data.user);
-    const newNamespace = socket.nsp;
-    const isReady = await this.redis.sismember(
-      `game:ready#${socket.nsp.name}`,
-      user,
-    );
-    if (!isReady) {
-      await this.redis.sadd(`game:ready#${socket.nsp.name}`, user);
-      const members = await this.getGameRoomMemberList(
-        `game:ready#${socket.nsp.name}`,
-      );
-      newNamespace.emit(Event.READY, members);
-    }
-  }
-
-  // 방 준비X
-  @SubscribeMessage(Event.UNREADY)
-  async handleUnready(
-    @MessageBody() data: { user: UserProfile; gameNumber: number },
-    @ConnectedSocket() socket: Socket,
-  ) {
-    const user = JSON.stringify(data.user);
-    const newNamespace = socket.nsp;
-    const isReady = await this.redis.sismember(
-      `game:ready#${socket.nsp.name}`,
-      user,
-    );
-    if (isReady) {
-      await this.redis.srem(`game:ready#${socket.nsp.name}`, user);
-      const members = await this.getGameRoomMemberList(
-        `game:ready#${socket.nsp.name}`,
-      );
-      newNamespace.emit(Event.READY, members);
-    }
-  }
-
-  // 시작
-  @SubscribeMessage(Event.START)
-  async handleStart(
-    @MessageBody() data: { user: UserProfile; gameNumber: number },
-    @ConnectedSocket() socket: Socket,
-  ) {
-    const newNamespace = socket.nsp;
-    const leader: UserProfileInGame = JSON.parse(
-      await this.redis.lindex(
-        `${GamePrefix.gameRoomMembers}${data.gameNumber}`,
-        0,
-      ),
-    );
-    if (data.user.id === leader.userId) {
-      const readyCount = await this.redis.scard(
-        `game:ready#${socket.nsp.name}`,
-      );
-      const memberCount = await this.redis.llen(
-        `${GamePrefix.gameRoomMembers}${data.gameNumber}`,
-      );
-      if (memberCount === readyCount + 1) {
-        newNamespace.emit(Event.START, {
-          start: true,
-          gameNumber: data.gameNumber,
-        });
-      } else {
-        newNamespace.emit(Event.START, {
-          start: false,
-          gameNumber: data.gameNumber,
-        });
-      }
-    } else {
-      newNamespace.emit(Event.START, {
-        start: false,
-        gameNumber: data.gameNumber,
-      });
-    }
-  }
-
-  // 메세지
-  @SubscribeMessage(Event.MESSAGE)
-  async handleMessage(
-    @MessageBody() data: { message: object },
-    @ConnectedSocket() socket: Socket,
-  ) {
-    socket.nsp.emit(Event.MESSAGE, data.message);
-  }
-
-  // 서버 메세지
-  @SubscribeMessage(Event.SERVER_MESSAGE)
-  async handleServerMessage(
-    @MessageBody() data: { message: object },
-    @ConnectedSocket() socket: Socket,
-  ) {
-    socket.nsp.emit(Event.SERVER_MESSAGE, data.message);
-  }
   // socket이 연결됐을 때
   async handleConnection(@ConnectedSocket() socket: Socket) {
     // 생성될 때 소켓 인스턴스의 namespace,  id
