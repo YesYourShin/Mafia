4cbe31dcb0a3804dc28ddaadad64b4f18b9616b7
seon0522 <iabc0005@daum.net>
2022-04-16 20:52:50 +0900

    게임 방 filed

--- src/modules/gateway/game-room/constants/game-redis-key-prefix .ts
+++ src/modules/gateway/game-room/constants/game-redis-key-prefix .ts
@@ -1,4 +1,3 @@
 export const GAME_ROOM = 'game-room';
-export const INFO_FIELD = 'info';
-export const MEMBER_FIELD = 'member';
-export const ROOM_NUMBER = 'daily-room-number';
+export const GAME_INFO_FIELD = 'game-info';
+export const GAME_MEMBER_FIELD = 'game-member';
