daf27b2f43dcbf4cb81f228154450742cb19e2c2
MJoon-Jung <gjgjajaj31@gmail.com>
2022-04-20 19:43:52 +0900

    [FIX] AWS s3 접근 불가 문제

--- src/shared/s3.ts
+++ src/shared/s3.ts
@@ -1,7 +1,20 @@
-import AWS from 'aws-sdk';
+import * as dotenv from 'dotenv';
 
-export const s3 = new AWS.S3({
+const envPath =
+  process.env.NODE_ENV === 'production'
+    ? '.env.production'
+    : process.env.NODE_ENV === 'testing'
+    ? '.env.testing'
+    : '.env.development';
+
+dotenv.config({ path: envPath });
+
+import * as AWS from 'aws-sdk';
+
+AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_S3_REGION,
+  region: process.env.AWS_REGION,
 });
+
+export const s3 = new AWS.S3();
