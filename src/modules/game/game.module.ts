140e2c4a93f3a0c2431c88667151e23a1c5dfbed
seon0522 <iabc0005@daum.net>
2022-05-11 15:45:00 +0900

    d

--- src/modules/game/game.module.ts
+++ src/modules/game/game.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { GameController } from './game.controller';
+import { GameRepository } from './game.repository';
 import { GameService } from './game.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([GameRepository])],
   controllers: [GameController],
   providers: [GameService],
 })
---
+++ src/modules/game/game.repository.ts
@@ -0,0 +1,13 @@
+import { Game, GameRole } from 'src/entities';
+import { AbstractRepository, getConnection, Repository } from 'typeorm';
+
+export class GameRepository extends AbstractRepository<Game> {
+  async save(UserProfile, roomId) {
+    // await getConnection()
+    //   .createQueryBuilder()
+    //   .insert()
+    //   .into(Game)
+    // //   .values()
+    //   .execute();
+  }
+}
