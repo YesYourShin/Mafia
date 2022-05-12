fec25befe7f8016d8899dde361dd0d7f3a6203fb
MJoon-Jung <gjgjajaj31@gmail.com>
2022-05-12 11:13:09 +0900

    [MODIFY] notification findAll pagination 삭제

--- src/modules/notification/notification.repository.ts
+++ src/modules/notification/notification.repository.ts
@@ -22,7 +22,7 @@ export class NotificationRepository extends AbstractRepository<Notification> {
       .execute();
   }
 
-  async findAll(targetId: number, page: number, perPage: number) {
+  async findAll(targetId: number) {
     const result = await getConnection()
       .createQueryBuilder()
       .from(Notification, 'notification')
@@ -37,13 +37,34 @@ export class NotificationRepository extends AbstractRepository<Notification> {
       ])
       .where('notification.targetId = :targetId', { targetId })
       .andWhere('notification.read = :read', { read: false })
-      .take(perPage)
-      .skip(perPage * (page - 1))
       .orderBy('notification.createdAt', 'DESC')
-      .getManyAndCount();
+      .getMany();
 
-    return { items: result[0], totalItems: result[1] };
+    return result;
   }
+
+  // async findAll(targetId: number, page: number, perPage: number) {
+  //   const result = await getConnection()
+  //     .createQueryBuilder()
+  //     .from(Notification, 'notification')
+  //     .select([
+  //       'notification.uuid',
+  //       'notification.type',
+  //       'notification.data',
+  //       'notification.read',
+  //       'notification.userId',
+  //       'notification.targetId',
+  //       'notification.createdAt',
+  //     ])
+  //     .where('notification.targetId = :targetId', { targetId })
+  //     .andWhere('notification.read = :read', { read: false })
+  //     .take(perPage)
+  //     .skip(perPage * (page - 1))
+  //     .orderBy('notification.createdAt', 'DESC')
+  //     .getManyAndCount();
+
+  //   return { items: result[0], totalItems: result[1] };
+  // }
 
   async findOne(uuid: string) {
     return await getConnection()
